a07fefa24a9dbe67c55a5f35cf68f8cc1a93b899
Bojan Dragojevic <bdragojevic@gmail.com>
2013-05-27 02:01:13 +0200

    initial push of project

---
+++ js/misc.js
@@ -0,0 +1,19 @@
+$(".selectpicker").select2(); 
+$('.nav').onePageNav({
+    currentClass: 'current',
+    changeHash: false,
+    scrollSpeed: 750,
+    scrollOffset: -1,
+    scrollThreshold: 0.5,
+    filter: '',
+    easing: 'swing',
+    begin: function() {
+        //I get fired when the animation is starting
+    },
+    end: function() {
+        //I get fired when the animation is ending
+    },
+    scrollChange: function($currentListItem) {
+        //I get fired when you enter a section and I pass the list item of the section
+    }
+});
