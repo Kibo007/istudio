0db69ecc11c4cab3c72fe2a35b2292afc7f09705
Bojan Dragojevic <bdragojevic@gmail.com>
2013-05-28 00:56:30 +0200

    validation form javascript added

--- js/misc.js
+++ js/misc.js
@@ -27,32 +27,120 @@ function pageNavigation() {
     });
 }
 
-//INPUT CHACKER
+//INPUT CHACKER 
 function inputChecker() {
     var inputs_leng = $('.input, textarea').length,
         submit = $('#submit'),
         inputs = $('.input, textarea');
 
-    inputs.change(function() {
-        var itemcount = 0;
+    inputs.blur(function() {
+        var itemcount = 0,
+            el = $(this);
 
-        inputs.each(function(i,input) {
+        if ( inputValidator(el) ) { 
 
-            if(input.value.length > 0) {
-                itemcount++;
+            inputs.each(function(i,input) {
+
+                if(input.value.length > 0) {
+                    itemcount++;
+                }
+            });
+
+            if(itemcount == inputs_leng) {
+                submit.addClass('enabled').removeClass('disabled');
+            } else {
+                disable_btn();
             }
-        });
-
-        if(itemcount == inputs_leng) {
-            submit.removeClass('disabled').addClass('enabled');
         } else {
-            submit.removeClass('enabled').addClass('disabled');
+            disable_btn();
         }
     });
+
+    function disable_btn() {
+        submit.removeClass('enabled').addClass('disabled');
+    }
 }
+
+//INPUT VALIDATOR
+function inputValidator(input) {
+
+    var id = input.attr('id');
+
+    if ( id == 'mail' ) {
+
+        if ( !input.val() == '' && validateEmail( input.val() ) ) {
+            pass_class(input);
+            return true
+        } else {
+            error_class(input);
+            return false
+        }
+
+    } else if ( id == 'telephone' ) {
+
+        if ( !input.val() == '' && validatePhone( input.val() ) ) {
+            pass_class(input);
+            return true
+        } else {
+            error_class(input);
+            return false
+        }
+
+    } else {
+
+        if (input.val() == '' ) { 
+            error_class(input);
+            return false
+        } else {
+            pass_class(input);
+            return true
+        }
+
+    }
+
+    //INPUT NOT PASS VALIDATION ADD ERROR CLASS
+    function error_class(el) {
+        el.closest('.input-prepend').addClass('error');
+    }
+
+    //VALIDATION PASS, ADD VALID FUNCTION
+    function pass_class(el) {
+        el.closest('.input-prepend').removeClass('error');
+        
+        if ( el.attr('id') !== 'name' ) { 
+            el.closest('.input-prepend').addClass('valid');
+        }
+    }
+
+
+    //MAIL VALIDATOR REGEX
+    function validateEmail($email) {
+        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+        if( !emailReg.test( $email ) ) {
+        return false;
+        } else {
+        return true;
+        }
+    }
+
+    //PHONE VALIDATOR REGEX
+    function validatePhone($phone) {
+        var phoneReg = /^[0-9-+]+$/;
+        if( !phoneReg.test( $phone ) ) {
+        return false;
+        } else {
+        return true;
+        }
+    }
+
+}
+
 
 //ON DOCUMENT READY INIT
 $(document).ready(function() {
     init_istudio();
 });
+
+
+
 
