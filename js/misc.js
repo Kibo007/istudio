76bde466a6dbadabdb698818ff10fc5460732ed5
Bojan Dragojevic <bdragojevic@gmail.com>
2013-05-27 19:19:46 +0200

    added javascript for enable disable btn

--- js/misc.js
+++ js/misc.js
@@ -1,19 +1,56 @@
-$(".selectpicker").select2(); 
-$('.nav').onePageNav({
-    currentClass: 'current',
-    changeHash: false,
-    scrollSpeed: 750,
-    scrollOffset: -1,
-    scrollThreshold: 0.5,
-    filter: '',
-    easing: 'swing',
-    begin: function() {
-        //I get fired when the animation is starting
-    },
-    end: function() {
-        //I get fired when the animation is ending
-    },
-    scrollChange: function($currentListItem) {
-        //I get fired when you enter a section and I pass the list item of the section
-    }
+//INIT FUNCTIONS
+function init_istudio() {
+    $(".selectpicker").select2(); 
+    inputChecker();
+    pageNavigation();
+}
+
+// PAGE NAVIGATION
+function pageNavigation() { 
+    $('.nav').onePageNav({
+        currentClass: 'current',
+        changeHash: false,
+        scrollSpeed: 750,
+        scrollOffset: -1,
+        scrollThreshold: 0.5,
+        filter: '',
+        easing: 'swing',
+        begin: function() {
+            //I get fired when the animation is starting
+        },
+        end: function() {
+            //I get fired when the animation is ending
+        },
+        scrollChange: function($currentListItem) {
+            //I get fired when you enter a section and I pass the list item of the section
+        }
+    });
+}
+
+//INPUT CHACKER
+function inputChecker() {
+    var firstinputs = $('.input, textarea').length;
+
+    $('.input, textarea').change(function() {
+        var itemcount = 0;
+
+        $('.input, textarea').each(function(i,input) {
+
+            if(input.value.length > 0) {
+                itemcount++;
+            }
+        });
+
+        if(itemcount == firstinputs) {
+            $('#submit').removeClass('disabled').addClass('enabled');
+        } else {
+            $('#submit').removeClass('enabled').addClass('disabled');
+        }
+    });
+}
+
+//ON DOCUMENT READY INIT
+$(document).ready(function() {
+    init_istudio();
 });
+
